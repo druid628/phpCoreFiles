547f5d7f6e1f52150a62d205e2afc2349f9c4839
Micah Breedlove <druid628@gmail.com>
2012-03-05 15:47:30 -0600

    updated phpdoc for 2 new functions

--- lib/com/druid628/BaseClass628.php
+++ lib/com/druid628/BaseClass628.php
@@ -201,11 +201,28 @@ abstract class BaseClass628 {
                 return $whoa;
         }
 
+        /**
+         *
+         * tests to see if class is being executed from command line
+         * 
+         * @return boolean
+         */
         public function isCli() 
         {
             return php_sapi_name()==="cli";
         }
 
+
+        /**
+         * Generates a humorous error message formed similar to the old
+         * DOS style errors
+         *
+         * @param String $errorType  - self explanitory e.g. Syntax
+         * @param String $message - explination error
+         * @param String $correction - what you should have put
+         *
+         * @return string
+         */
         public function formatErrorDOS($errorType, $message, $correction)
         {
             $errorType = strtoupper($errorType);
